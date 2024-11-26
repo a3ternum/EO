5ed76012975d8f23e8a643ff6db86f7476b15776
a3ternum <sena.ozdemir.1@gmail.com>
2024-11-26 21:41:28 +0100

    updated assembly, removed playerTest and commented out mapGeneratorEditor since it fucks with build right now

--- Assets/Scripts/Procedural/Editor/RandomMapGeneratorEditor.cs
+++ Assets/Scripts/Procedural/Editor/RandomMapGeneratorEditor.cs
@@ -1,24 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
 
-[CustomEditor(typeof(AbstractMapGenerator), true)]
-public class RandomMapGeneratorEditor : Editor
-{
-    AbstractMapGenerator generator;
+//[CustomEditor(typeof(AbstractMapGenerator), true)]
+//public class RandomMapGeneratorEditor : Editor
+//{
+//    AbstractMapGenerator generator;
 
-    private void Awake()
-    {
-        generator = (AbstractMapGenerator)target;
-    }
+//    private void Awake()
+//    {
+//        generator = (AbstractMapGenerator)target;
+//    }
 
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-        if (GUILayout.Button("Create Map"))
-        {
-            generator.GenerateMap();
-        }
-    }
-}
+//    public override void OnInspectorGUI()
+//    {
+//        base.OnInspectorGUI();
+//        if (GUILayout.Button("Create Map"))
+//        {
+//            generator.GenerateMap();
+//        }
+//    }
+//}
