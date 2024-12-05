42576c5e695dbfed120b10b4ebd0b738690e5e14
a3ternum <sena.ozdemir.1@gmail.com>
2024-12-05 16:08:34 +0100

    random shit

--- Assets/Scripts/UI/Inventory/InventoryController.cs
+++ Assets/Scripts/UI/Inventory/InventoryController.cs
@@ -1,36 +1,178 @@
+using Inventory.Model;
+using Inventory.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
-public class InventoryController : MonoBehaviour
+namespace Inventory
 {
-    [SerializeField]
-    private InventoryPage inventoryPage;
-
-    public int inventorySize = 54;
-    private void Start()
+    public class InventoryController : MonoBehaviour
     {
-        inventoryPage.InitializeInventory(inventorySize);
-    }
+        [SerializeField]
+        private UIInventoryPage inventoryUI;
 
+        [SerializeField]
+        private InventorySO inventoryData;
 
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.I))
+        public List<InventoryItem> initialItems = new List<InventoryItem>();
+
+        [SerializeField]
+        private AudioClip dropClip;
+
+        [SerializeField]
+        private AudioSource audioSource;
+
+        private void Start()
         {
-            if (inventoryPage.isActiveAndEnabled == false)
+            PrepareUI();
+            PrepareInventoryData();
+        }
+
+        private void PrepareInventoryData()
+        {
+            inventoryData.Initialize();
+            inventoryData.OnInventoryUpdated += UpdateInventoryUI;
+            foreach (InventoryItem item in initialItems)
             {
-                inventoryPage.Show();
-            }
-            else
-            {
-                inventoryPage.Hide();
+                if (item.IsEmpty)
+                    continue;
+                inventoryData.AddItem(item);
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.Escape))
+        private void UpdateInventoryUI(Dictionary<int, InventoryItem> inventoryState)
         {
-            if (inventoryPage.isActiveAndEnabled == true)
+            inventoryUI.ResetAllItems();
+            foreach (var item in inventoryState)
             {
-                inventoryPage.Hide();
+                inventoryUI.UpdateData(item.Key, item.Value.item.ItemImage, 
+                    item.Value.quantity);
+            }
+        }
+
+        private void PrepareUI()
+        {
+            inventoryUI.InitializeInventoryUI(inventoryData.Size);
+            inventoryUI.OnDescriptionRequested += HandleDescriptionRequest;
+            inventoryUI.OnSwapItems += HandleSwapItems;
+            inventoryUI.OnStartDragging += HandleDragging;
+            inventoryUI.OnItemActionRequested += HandleItemActionRequest;
+        }
+
+        private void HandleItemActionRequest(int itemIndex)
+        {
+            InventoryItem inventoryItem = inventoryData.GetItemAt(itemIndex);
+            if (inventoryItem.IsEmpty)
+                return;
+
+            IItemAction itemAction = inventoryItem.item as IItemAction;
+            if(itemAction != null)
+            {
+                
+                inventoryUI.ShowItemAction(itemIndex);
+                inventoryUI.AddAction(itemAction.ActionName, () => PerformAction(itemIndex));
+            }
+
+            IDestroyableItem destroyableItem = inventoryItem.item as IDestroyableItem;
+            if (destroyableItem != null)
+            {
+                inventoryUI.AddAction("Drop", () => DropItem(itemIndex, inventoryItem.quantity));
+            }
+
+        }
+
+        private void DropItem(int itemIndex, int quantity)
+        {
+            inventoryData.RemoveItem(itemIndex, quantity);
+            inventoryUI.ResetSelection();
+            audioSource.PlayOneShot(dropClip);
+        }
+
+        public void PerformAction(int itemIndex)
+        {
+            InventoryItem inventoryItem = inventoryData.GetItemAt(itemIndex);
+            if (inventoryItem.IsEmpty)
+                return;
+
+            IDestroyableItem destroyableItem = inventoryItem.item as IDestroyableItem;
+            if (destroyableItem != null)
+            {
+                inventoryData.RemoveItem(itemIndex, 1);
+            }
+
+            IItemAction itemAction = inventoryItem.item as IItemAction;
+            if (itemAction != null)
+            {
+                itemAction.PerformAction(gameObject, inventoryItem.itemState);
+                audioSource.PlayOneShot(itemAction.actionSFX);
+                if (inventoryData.GetItemAt(itemIndex).IsEmpty)
+                    inventoryUI.ResetSelection();
+            }
+        }
+
+        private void HandleDragging(int itemIndex)
+        {
+            InventoryItem inventoryItem = inventoryData.GetItemAt(itemIndex);
+            if (inventoryItem.IsEmpty)
+                return;
+            inventoryUI.CreateDraggedItem(inventoryItem.item.ItemImage, inventoryItem.quantity);
+        }
+
+        private void HandleSwapItems(int itemIndex_1, int itemIndex_2)
+        {
+            inventoryData.SwapItems(itemIndex_1, itemIndex_2);
+        }
+
+        private void HandleDescriptionRequest(int itemIndex)
+        {
+            InventoryItem inventoryItem = inventoryData.GetItemAt(itemIndex);
+            if (inventoryItem.IsEmpty)
+            {
+                inventoryUI.ResetSelection();
+                return;
+            }
+            ItemSO item = inventoryItem.item;
+            string description = PrepareDescription(inventoryItem);
+            inventoryUI.UpdateDescription(itemIndex, item.ItemImage,
+                item.name, description);
+        }
+
+        private string PrepareDescription(InventoryItem inventoryItem)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(inventoryItem.item.Description);
+            sb.AppendLine();
+            for (int i = 0; i < inventoryItem.itemState.Count; i++)
+            {
+                sb.Append($"{inventoryItem.itemState[i].itemParameter.ParameterName} " +
+                    $": {inventoryItem.itemState[i].value} / " +
+                    $"{inventoryItem.item.DefaultParametersList[i].value}");
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                if (inventoryUI.isActiveAndEnabled == false)
+                {
+                    inventoryUI.Show();
+                    foreach (var item in inventoryData.GetCurrentInventoryState())
+                    {
+                        inventoryUI.UpdateData(item.Key,
+                            item.Value.item.ItemImage,
+                            item.Value.quantity);
+                    }
+                }
+                else
+                {
+                    inventoryUI.Hide();
+                }
+
             }
         }
     }
--- Assets/Scripts/UI/Inventory/Model/itemSO.cs
+++ Assets/Scripts/UI/Inventory/Model/itemSO.cs
@@ -1,24 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Item", menuName = "Inventory/Item")]
-public class ItemSO : ScriptableObject
+namespace Inventory.Model
 {
-    [field: SerializeField]
-    public bool IsStackable { get; set; }
+    public abstract class ItemSO : ScriptableObject
+    {
+        [field: SerializeField]
+        public bool IsStackable { get; set; }
 
-    public int ID => GetInstanceID();
+        public int ID => GetInstanceID();
 
-    [field: SerializeField]
-    public int MaxStackSize { get; set; } = 1;
-    [field: SerializeField]
-    public string itemName { get; set; }
+        [field: SerializeField]
+        public int MaxStackSize { get; set; } = 1;
 
-    [field: SerializeField]
-    [field: TextArea]
-    public string Description { get; set; }
+        [field: SerializeField]
+        public string Name { get; set; }
 
-    [field: SerializeField]
-    public Sprite ItemImage { get; set; }
+        [field: SerializeField]
+        [field: TextArea]
+        public string Description { get; set; }
 
+        [field: SerializeField]
+        public Sprite ItemImage { get; set; }
 
+        [field: SerializeField]
+        public List<ItemParameter> DefaultParametersList { get; set; }
+
+    }
+
+    [Serializable]
+    public struct ItemParameter : IEquatable<ItemParameter>
+    {
+        public ItemParameterSO itemParameter;
+        public float value;
+
+        public bool Equals(ItemParameter other)
+        {
+            return other.itemParameter == itemParameter;
+        }
+    }
 }
+
