9a8dbc315d6db2f8427ed07a5234559cdf59de10
a3ternum <sena.ozdemir.1@gmail.com>
2025-03-02 16:50:37 +0100

    items on the ground now have a textbox above them that shows the items name and items stats when the player hovers his mouse over the item

--- Assets/Scripts/UI/Inventory/GroundItem.cs
+++ Assets/Scripts/UI/Inventory/GroundItem.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 using TMPro;
 using System.Threading;
 
-public class GroundItem : MonoBehaviour
+public class GroundItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [Header("UI")]
     private TextMeshPro descriptionText;
+    private GroundItemTextBox groundTextBox;
 
+    private BoxCollider2D textBoxCollider;
     public Item item;
     public int stackSize;
 
@@ -30,11 +32,23 @@ public class GroundItem : MonoBehaviour
         Canvas.ForceUpdateCanvases(); // update the canvas to get the correct bounds of the text
 
         // add a collider to the description text to make it clickable
-        BoxCollider2D collider = descriptionText.gameObject.AddComponent<BoxCollider2D>();
-        collider.size = new Vector2(descriptionText.bounds.size.x, descriptionText.bounds.size.y);
+        textBoxCollider = descriptionText.gameObject.AddComponent<BoxCollider2D>();
+
+        textBoxCollider.size = new Vector2(descriptionText.bounds.size.x, descriptionText.bounds.size.y);
+
+        // instantiate the ground item text box prefab
+        groundTextBox = Instantiate(Resources.Load<GroundItemTextBox>("Inventory/GroundItemTextBox"), transform, false);
+        groundTextBox.transform.SetParent(transform); // set the groundTextBox as a child of the ground item
+        // move the ground item text box to the top of the ground item
+        groundTextBox.transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+
+        groundTextBox.itemName.text = item.itemName;
+        groundTextBox.itemStats.text = item.itemStats;
+        Debug.Log("ground item stats: " + groundTextBox.itemStats);
+        groundTextBox.gameObject.SetActive(false); // hide the ground item text box
     }
 
-   
+
 
     private void OnMouseDown()
     {
@@ -54,4 +68,24 @@ public class GroundItem : MonoBehaviour
         Destroy(gameObject); // destroy the gameobject after picking it up
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("in OnPointerEnter on ground item");
+        Debug.Log("groundtextbox: " + groundTextBox);
+        // activate the groundTextBox prefab of the item
+        if (groundTextBox != null)
+        {
+            groundTextBox.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // deactivate the groundTextBox prefab of the item
+        if (groundTextBox != null)
+        {
+            groundTextBox.gameObject.SetActive(false);
+        }
+
+    }
 }
---
+++ Assets/Scripts/UI/Inventory/GroundItemTextBox.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using TMPro;
+
+public class GroundItemTextBox : MonoBehaviour
+{
+    public TextMeshPro itemName;
+    public TextMeshPro itemStats;
+}
